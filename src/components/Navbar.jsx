1e44f6331cd7802ce08a9cbad9b958b267dcc041
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-06-23 22:12:13 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,57 +1,3 @@
-// import React, { useState } from 'react';
-// import { NavLink } from 'react-router-dom';
-// import './navbar.css';
-// import { IoIosCart } from 'react-icons/io';
-// import { MdOutlineWbSunny } from "react-icons/md";
-// import { IoMoonOutline } from "react-icons/io5";
-
-// function Navbar() {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
-//   return (
-//     <div className="navbar">
-//       <div className="nav-logo">
-//         <h1>Cluster</h1>
-//       </div>
-
-//       <div className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
-//         <NavLink to="/" onClick={toggleMenu}>
-//           Home
-//         </NavLink>
-//         <NavLink to="/about" onClick={toggleMenu}>
-//           About
-//         </NavLink>
-//         <NavLink to="/contact" onClick={toggleMenu}>
-//           Contact
-//         </NavLink>
-//         <NavLink to="/products" onClick={toggleMenu}>
-//           Menu
-//         </NavLink>
-//         <NavLink to="/cart" onClick={toggleMenu}>
-//           <IoIosCart className="nav-icon" />
-//         </NavLink>
-
-//         <button onClick={toggleDarkMode} className="theme-toggle-btn">
-//           {darkMode ? <MdOutlineWbSunny /> : <IoMoonOutline />}
-//         </button>
-//       </div>
-
-//       <div className="menu-toggle" onClick={toggleMenu}>
-//         <div className={`bar ${isMenuOpen ? 'open' : ''}`} />
-//         <div className={`bar ${isMenuOpen ? 'open' : ''}`} />
-//         <div className={`bar ${isMenuOpen ? 'open' : ''}`} />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Navbar;
-
-
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import './navbar.css';
@@ -107,9 +53,6 @@ function Navbar({ darkMode, toggleDarkMode }) {
           <IoIosCart className="nav-icon" />
           <span className="cart-count">{cartCount}</span>
         </NavLink>
-        {/* <button onClick={toggleDarkMode} className="theme-toggle-btn">
-          {darkMode ? <MdOutlineWbSunny /> : <IoMoonOutline />}
-        </button> */}
       </div>
 
 
