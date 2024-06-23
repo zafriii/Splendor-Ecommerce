c8d1280cce3bf452921afcf865ff6dce11d6a720
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-06-23 22:11:35 +0600

    Update FashionHeader.jsx

--- src/components/FashionHeader.jsx
+++ src/components/FashionHeader.jsx
@@ -5,20 +5,10 @@ function FashionHeader() {
   return (
     <div className="header-container">
     <div className="text-container">
-        <h1 className="main-heading">New Fashion</h1>
-         <p className="description">
-            {/* New Fashion */}
-        </p> 
-        
-        {/* <button className="shop-now-button">Shop Now</button> */}
+        <h2 className="main-heading">New Fashion</h2>        
     </div>
     <div className="image-container">
         <img src="images/pink.png" alt="Fashion Model" className="main-image" />
-        {/* <div className="product-info bottom-product">
-            <img src="https://via.placeholder.com/50" alt="Product 2" />
-            <p>Solidies Kredezug Satin-Minikleid - $21.00</p>
-            <div className="rating">⭐⭐⭐⭐⭐</div>
-        </div> */}
     </div>
 </div>
   )
